44a3eeb07cbf1187853f066430fc3342a24e3380
Joseph Perkins <gamerdew64@gmail.com>
2018-10-17 14:17:35 -0500

    updated app.js filee and component files -> app now pulls NYT headlines and summaries along with word counts

--- client/src/App.js
+++ client/src/App.js
@@ -26,26 +26,10 @@ class App extends Component {
     // When the form is submitted, prevent its default behavior, get recipes update the recipes state
     event.preventDefault();
     API.getRecipes(this.state.recipeSearch)
-      .then(res => this.setState({ recipes: res.data.response.docs }))
+      // .then(res => this.setState({ recipes: res.data.response.docs }))
+      .then(res => this.setState({ recipes: res.data }))
       .catch(err => console.log(err));
   };
-
-  // // Get specific article info for current index
-  // var article = data.response.docs;
-  // console.log(data.response.docs);
-  //
-  // // If the article has a byline, log and append to $articleList
-  // var byline = article.byline.original;
-  // console.log(article.byline.original);
-  //
-  // // Log section, and append to document if exists
-  // var section = article.section_name;
-  // console.log(article.section_name);
-  //
-  // // Log published date, and append to document if exists
-  // var pubDate = article.pub_date;
-  // console.log(article.pub_date);
-
 
   render() {
     return (
@@ -89,11 +73,12 @@ class App extends Component {
                   {this.state.recipes.map(recipe => {
                     return (
                       <RecipeListItem
-                        key={recipe.data.response.docs.byline.original}
-                        title={recipe.data.response.docs.byline.original}
-                        href={recipe.data.response.docs.section_name}
-                        ingredients={recipe.data.response.docs.pub_date}
-                        thumbnail={recipe.thumbnail}
+                        key={recipe._id}
+                        title={recipe.headline.main}
+                        href={recipe.web_url}
+                        ingredients={recipe.snippet}
+                        thumbnail={recipe.multimedia.url}
+                        wordcount={recipe.word_count}
                       />
                     );
                   })}
@@ -108,99 +93,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-// import React, { Component } from "react";
-// import Jumbotron from "./components/Jumbotron";
-// import Nav from "./components/Nav";
-// import Input from "./components/Input";
-// import Button from "./components/Button";
-// import API from "./utils/API";
-// import { RecipeList, RecipeListItem } from "./components/RecipeList";
-// import { Container, Row, Col } from "./components/Grid";
-//
-// class App extends Component {
-//   state = {
-//     recipes: [],
-//     recipeSearch: ""
-//   };
-//
-//   handleInputChange = event => {
-//     // Destructure the name and value properties off of event.target
-//     // Update the appropriate state
-//     const { name, value } = event.target;
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-//
-//   handleFormSubmit = event => {
-//     // When the form is submitted, prevent its default behavior, get recipes update the recipes state
-//     event.preventDefault();
-//     API.getRecipes(this.state.recipeSearch)
-//       .then(res => this.setState({ recipes: res.data }))
-//       .catch(err => console.log(err));
-//   };
-//
-//   render() {
-//     return (
-//       <div>
-//         <Nav />
-//         <Jumbotron />
-//         <Container>
-//           <Row>
-//             <Col size="md-12">
-//               <form>
-//                 <Container>
-//                   <Row>
-//                     <Col size="xs-9 sm-10">
-//                       <Input
-//                         name="recipeSearch"
-//                         value={this.state.recipeSearch}
-//                         onChange={this.handleInputChange}
-//                         placeholder="Search For a Recipe"
-//                       />
-//                     </Col>
-//                     <Col size="xs-3 sm-2">
-//                       <Button
-//                         onClick={this.handleFormSubmit}
-//                         type="success"
-//                         className="input-lg"
-//                       >
-//                         Search
-//                       </Button>
-//                     </Col>
-//                   </Row>
-//                 </Container>
-//               </form>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Col size="xs-12">
-//               {!this.state.recipes.length ? (
-//                 <h1 className="text-center">No Recipes to Display</h1>
-//               ) : (
-//                 <RecipeList>
-//                   {this.state.recipes.map(recipe => {
-//                     return (
-//                       <RecipeListItem
-//                         key={recipe.title}
-//                         title={recipe.title}
-//                         href={recipe.href}
-//                         ingredients={recipe.ingredients}
-//                         thumbnail={recipe.thumbnail}
-//                       />
-//                     );
-//                   })}
-//                 </RecipeList>
-//               )}
-//             </Col>
-//           </Row>
-//         </Container>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
--- client/src/components/RecipeList/RecipeListItem.js
+++ client/src/components/RecipeList/RecipeListItem.js
@@ -8,14 +8,13 @@ export const RecipeListItem = props => (
     <Container>
       <Row>
         <Col size="xs-4 sm-2">
-          <Thumbnail src={props.thumbnail || "https://placehold.it/300x300"} />
+          <Thumbnail src={props.thumbnail || "http://www.prescottlibrary.info/wp-content/uploads/2018/07/New-York-Times-Logo-300x300.jpg"} />
         </Col>
         <Col size="xs-8 sm-9">
           <h3>{props.title}</h3>
-          <p>Ingredients: {props.ingredients}</p>
-          <a rel="noreferrer noopener" target="_blank" href={props.href}>
-            Go to recipe!
-          </a>
+          <p><b>Summary:</b> {props.ingredients}</p>
+          <p>Word Count: {props.wordcount}</p>
+          <a rel="noreferrer noopener" target="_blank" href={props.href}>Take me to the link!</a>
         </Col>
       </Row>
     </Container>
